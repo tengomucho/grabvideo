b4197ed7d9486a0983291472d3de16f8dc7d5b00
Alvaro Moran <alvaro.moran@huggingface.co>
2025-02-28 12:00:24 +0100

    feat: first functional version
    This can download yt-dlp and download a video by launching the Terminal.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Grabvideo
+
+This is a very simple GUI wrapper for [yt-dlp](https://github.com/yt-dlp/yt-dlp) video for macOS.
+
+I built this for a teacher that is not familiar with the terminal but needs sometimes to grab a video from the internet to show it to her pupils.
+
+The code is based on [Tauri](https://tauri.app/), Rust + HTML.
+
---
+++ src/main.js
@@ -0,0 +1,57 @@
+const { invoke } = window.__TAURI__.core;
+
+let downloadInputEl;
+let downloadMsgEl;
+
+async function download() {
+  var status = document.getElementById("setup-status");
+  status.style.opacity = 1;
+  status.textContent = "Downloading...";
+  var download_status = await invoke("video_download", { url: downloadInputEl.value });
+  if (download_status) {
+    status.textContent = "Download completed.";
+    setTimeout(() => {
+      status.classList.add("fade-out");
+    }, 1000);
+  } else {
+    status.textContent = "⚠️ Failed to download video. ⚠️";
+    status.style.color = "red";
+  }
+}
+
+async function check_setup() {
+  var status = document.getElementById("setup-status");
+  status.textContent = "Getting ready...";
+  var setup_ready = await invoke("check_yt_dlp_setup");
+
+  function tell_setup_ready() {
+    status.textContent = "Setup ready.";
+    setTimeout(() => {
+      status.classList.add("fade-out");
+    }, 1000);
+  }
+
+  if (setup_ready) {
+    tell_setup_ready()
+    return;
+  }
+  status.textContent = "Setting up dependencies (downloading )...";
+
+  var install_ready = await invoke("install_yt_dlp");
+  if (install_ready) {
+    tell_setup_ready()
+    return;
+  }
+  status.textContent = "⚠️ Failed to setup dependencies, try closing and reopening the app. ⚠️";
+  status.style.color = "red";
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  downloadInputEl = document.querySelector("#download-input");
+  document.querySelector("#download-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    download();
+  });
+
+  check_setup();
+});
